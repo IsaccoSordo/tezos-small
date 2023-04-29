5bd1bb449c8083232f56d7c099876d73c68de89a
Isacco <isacco.sordo@gmail.com>
2023-04-29 13:15:21 +0200

    feat: added blocks-overview tests

--- src/app/blocks-overview/blocks-overview.component.spec.ts
+++ src/app/blocks-overview/blocks-overview.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockSelector, provideMockStore } from '@ngrx/store/testing';
 import { BlocksOverviewComponent } from './blocks-overview.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { selectBlocks, selectBlocksCount } from '../store/tzkt.selectors';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BlocksOverviewComponent', () => {
   let component: BlocksOverviewComponent;
   let fixture: ComponentFixture<BlocksOverviewComponent>;
+  const selectors: MockSelector[] = [
+    {
+      selector: selectBlocks,
+      value: [{ level: 123 }],
+    },
+    {
+      selector: selectBlocksCount,
+      value: 123,
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NgbModule],
-      declarations: [ BlocksOverviewComponent ],
-      providers: [provideMockStore({})]
-    })
-    .compileComponents();
+      imports: [NgbModule, RouterTestingModule.withRoutes([])],
+      declarations: [BlocksOverviewComponent],
+      providers: [provideMockStore({ selectors: selectors })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BlocksOverviewComponent);
     component = fixture.componentInstance;
@@ -22,5 +33,17 @@ describe('BlocksOverviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run ngOnInit', (done) => {
+    component.ngOnInit();
+    component.blocks$.subscribe((data) => {
+      expect(data).toHaveSize(1);
+      expect(data[0].level).toEqual(123);
+      done();
+    });
+    component.count$.subscribe((data) => {
+      expect(data).toEqual(123);
+    });
   });
 });
