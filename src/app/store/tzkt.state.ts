244045b3b252d5fa877828c8383bcfd5e5a0cbc1
Isacco <isacco.sordo@gmail.com>
2023-08-30 21:38:57 +0200

    fix: added comment

--- src/app/store/tzkt.state.ts
+++ src/app/store/tzkt.state.ts
@@ -1,6 +1,8 @@
 import { WritableSignal } from '@angular/core';
 import { Block, Transaction } from '../common';
 
+// as for https://github.com/ng-bootstrap/ng-bootstrap/issues/4556
+// in order for ngb-alert to display an error such error must be an object
 export interface Error {
   text: string;
 }
